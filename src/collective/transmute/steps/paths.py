b0c84121289442b744cf8f114466c019daa729eb
Érico Andrei <ericof@gmail.com>
2025-02-24 22:46:51 -0300

    Add drop to paths step

--- src/collective/transmute/steps/paths.py
+++ src/collective/transmute/steps/paths.py
@@ -4,6 +4,10 @@ from collective.transmute import _types as t
 def _is_valid_path(path_filter: dict, path: str) -> bool:
     """Check if path is allowed to be processed."""
     status = True
+    if drop := path_filter.get("drop", []):
+        for prefix in drop:
+            if path.startswith(prefix):
+                return False
     if allowed := path_filter.get("allowed", []):
         status = False
         for prefix in allowed:
